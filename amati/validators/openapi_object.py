357779b65b3b8e8db116edea969f0904e073f8d2
Ben Alexander <ben@benjamins-mbp.home>
2024-12-08 21:23:12 +0000

    Add in top-level OpenAPI Object; remove HTTP verbs as these are not part of the spec in the implemented manner

--- amati/validators/openapi_object.py
+++ amati/validators/openapi_object.py
@@ -13,11 +13,15 @@ from amati.validators.info_object import InfoObject
 OPENAPI_VERSIONS = ['1.0', '1.1', '1.2', '2.0', '3.0', '3.0.1', '3.0.2', '3.0.3', '3.1', '3.1.1']
 
 
+def _validate_after_openapi(value: str) -> str:
+    if value not in OPENAPI_VERSIONS:
+        raise ValueError(f"{value} is not a valid OpenAPI version.")
+    return value
 
 OpenAPI = Annotated[
     str,
     Field(strict=True), 
-    AfterValidator(lambda v: v in OPENAPI_VERSIONS)
+    AfterValidator(_validate_after_openapi)
     ]
 
 
--- tests/test_openapi_object.py
+++ tests/test_openapi_object.py
@@ -1,43 +1,45 @@
 """
-This will change significantly over time, and currently only serves
-as a placeholder to demonstrate that the strategy taken in this project
-theoretically works
+Tests amati/validators/openapi_object.py
 """
 
 import json
 import pytest
 import yaml
 
+from hypothesis import given, strategies as st
 from pydantic import ValidationError
 
 from amati.validators.generic import GenericObject
-from amati.validators.http_status_codes import HTTPStatusCode
-from amati.validators.http_verbs import HTTPVerb
-from amati.validators.info_object import InfoObject
+from amati.validators.openapi_object import OpenAPIObject, OpenAPI, OPENAPI_VERSIONS
 
 
-class HTTP(GenericObject):
-    verb: HTTPVerb
-    status: HTTPStatusCode
+class Model(GenericObject):
+    value: OpenAPI
 
 
-class Everything(GenericObject):
-    info: InfoObject
-    http: HTTP
-    http: HTTP
-
-
-def test_everything_works():
+def test_valid_openapi_object():
     with open('tests/data/good_spec.yaml', 'r', encoding='utf-8') as f:
         data = yaml.safe_load(f)
 
-    model = Everything(**data)
+    model = OpenAPIObject(**data)
     assert json.loads(model.model_dump_json(exclude_unset=True)) == data
 
 
-def test_everything_fails():
+def test_invalid_openapi_object():
     with open('tests/data/bad_spec.yaml', 'r', encoding='utf-8') as f:
         data = yaml.safe_load(f)
 
     with pytest.raises(ValidationError):
-        Everything(**data)
+        OpenAPIObject(**data)
+
+
+@given(st.text().filter(lambda x: x not in OPENAPI_VERSIONS))
+def test_invalid_openapi_version(value: str):
+    with pytest.raises(ValidationError):
+        Model(value=value)
+
+
+@given(st.sampled_from(OPENAPI_VERSIONS))
+def test_valid_openapi_version(value: str):
+    model = Model(value=value)
+    assert model.value == value
