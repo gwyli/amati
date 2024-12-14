4a8e73f8ae4b368ba3991ddf929365fb1532ba09
Ben Alexander <ben@benjamins-mbp.home>
2024-12-14 10:46:33 +0000

    Run Black + isort

--- scripts/spdx-licences.py
+++ scripts/spdx-licences.py
@@ -4,13 +4,16 @@ import pathlib
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://raw.githubusercontent.com/spdx/license-list-data/refs/heads/main/json/licenses.json")
+    response = requests.get(
+        "https://raw.githubusercontent.com/spdx/license-list-data/refs/heads/main/json/licenses.json"
+    )
     response.raise_for_status()  # Raise an exception for HTTP errors
-    
+
     data = json.dumps(response.json())
 
-    
-    write_to = pathlib.Path(__file__).parent.parent.resolve() / 'amati/data/spdx-licences.json'  
+    write_to = (
+        pathlib.Path(__file__).parent.parent.resolve() / "amati/data/spdx-licences.json"
+    )
 
     with write_to.open("w") as f:
         f.write(data)
