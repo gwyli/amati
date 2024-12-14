2745218e90b1a477f6c2d89464f72bd63f877990
Ben Alexander <ben@benjamins-mbp.home>
2024-12-14 13:16:08 +0000

    Run Black again

--- spdx_licences.py
+++ spdx_licences.py
@@ -9,8 +9,8 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get(
-        "https://raw.githubusercontent.com/spdx/license-list-data/refs/heads/main/json/licenses.json", # pylint: disable=ignore line-too-long
-        timeout=20
+        "https://raw.githubusercontent.com/spdx/license-list-data/refs/heads/main/json/licenses.json",  # pylint: disable=ignore line-too-long
+        timeout=20,
     )
     response.raise_for_status()  # Raise an exception for HTTP errors
 
