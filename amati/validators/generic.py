e73e05dfd69b97f22e87d5f9d4d217d43d5d671a
Ben <2551337+ben-alexander@users.noreply.github.com>
2025-04-17 16:58:21 +0100

    Rename _field_pattern to _extra_field_pattern

--- amati/validators/generic.py
+++ amati/validators/generic.py
@@ -30,9 +30,10 @@ class GenericObject(LogMixin, BaseModel):
     """
 
     _reference: ClassVar[Optional[Reference]] = PrivateAttr()
-    _pattern: Pattern[str] = PrivateAttr()
+    _extra_field_pattern: Optional[Pattern[str]] = PrivateAttr()
 
     def __init__(self, **data: Any) -> None:
+
         super().__init__(**data)
 
         if "extra" in self.model_config:
@@ -41,14 +42,14 @@ class GenericObject(LogMixin, BaseModel):
         # If extra fields aren't allowed log those that aren't going to be added
         # to the model.
         for field in data:
-            if field in self.model_fields:
-                continue
-            LogMixin.log(
-                Log(
-                    message=f"{field} is not a valid field for {self.__repr_name__()}.",
-                    type=ValueError,
+            if field not in self.model_fields:
+                message = f"{field} is not a valid field for {self.__repr_name__()}."
+                self.log(
+                    Log(
+                        message=message,
+                        type=ValueError,
+                    )
                 )
-            )
 
     def model_post_init(self, __context: Any) -> None:
         if not self.model_extra:
@@ -57,12 +58,12 @@ class GenericObject(LogMixin, BaseModel):
         excess_fields: set[str] = set()
 
         # Any extra fields are allowed
-        if self._pattern is None:
+        if self._extra_field_pattern is None:
             return
         else:
-            prog: Pattern[str] = re.compile(self._pattern)
+            pattern: Pattern[str] = re.compile(self._extra_field_pattern)
             excess_fields.update(
-                key for key in self.model_extra.keys() if not prog.match(key)
+                key for key in self.model_extra.keys() if not pattern.match(key)
             )
 
         for field in excess_fields:
@@ -80,12 +81,12 @@ T = TypeVar("T", bound=GenericObject)
 
 def allow_extra_fields(pattern: Optional[str] = None) -> Callable[[Type[T]], Type[T]]:
     """
-    A decorator that modifies a Pydantic BaseModel to allow extra fields
-    and optionally sets a pattern for specification extensions.
+    A decorator that modifies a Pydantic BaseModel to allow extra fields and optionally
+    sets a pattern for those extra fields
 
     Args:
-        pattern: Optional pattern string for specification extensions.
-        If not provided all extra fields will be allowed
+        pattern: Optional pattern string for extra fields. If not provided all extra
+        fields will be allowed
 
     Returns:
         A decorator function that adds a ConfigDict allowing extra fields
@@ -98,7 +99,7 @@ def allow_extra_fields(pattern: Optional[str] = None) -> Callable[[Type[T]], Typ
         """
         namespace: dict[str, Union[ConfigDict, Optional[str]]] = {
             "model_config": ConfigDict(extra="allow"),
-            "_pattern": pattern,
+            "_extra_field_pattern": pattern,
         }
         # Create a new class with the updated configuration
         return cast(Type[T], type(cls.__name__, (cls,), namespace))
