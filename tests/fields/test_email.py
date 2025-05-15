41d4e5b3932a2accd8bcbb5144dbc906d123e2b3
Ben <2551337+ben-alexander@users.noreply.github.com>
2025-05-15 11:49:22 +0100

    Correct regex to identify email like strings generated by Hypothesis

--- tests/fields/test_email.py
+++ tests/fields/test_email.py
@@ -35,7 +35,7 @@ def strings_except_emails(draw: st.DrawFn) -> str:
 
     # The Hypothesis string shrinking algorithm ends up producing a valid RFC 5322 email
     # email sometimes. Exclude them.
-    while re.match("[a-z0-9]@[a-z0-9]", candidate, flags=re.IGNORECASE):
+    while re.match("[a-z0-9]+@[a-z0-9]+", candidate, flags=re.IGNORECASE):
         candidate = draw(st.text())  # pragma: no cover
 
     return candidate
