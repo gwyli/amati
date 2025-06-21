06359f6e5bf0645ba10dd2f3b37993e7f7b20032
Ben <2551337+ben-alexander@users.noreply.github.com>
2025-06-21 13:22:09 +0100

    Add GitHub's JSON specification, fix issue with OpenAPI versions and add Tarjan's algorithm to do a topological sort to determine rebuild order as the ParameterObject was not getting rebuilt.

--- amati/fields/oas.py
+++ amati/fields/oas.py
@@ -12,11 +12,13 @@ from amati.grammars import oas
 
 OPENAPI_VERSIONS: list[str] = [
     "3.0",
+    "3.0.0",
     "3.0.1",
     "3.0.2",
     "3.0.3",
     "3.0.4",
     "3.1",
+    "3.1.0",
     "3.1.1",
 ]
 
