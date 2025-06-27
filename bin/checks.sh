a72f427781ecf6569a9f1c96a818681badb5446e
Ben <2551337+ben-alexander@users.noreply.github.com>
2025-06-28 00:05:45 +0100

    upgrade to Python 3.13.5

--- bin/checks.sh
+++ bin/checks.sh
@@ -5,6 +5,6 @@ python scripts/tests/setup_test_specs.py
 pytest --cov-report term-missing --cov=amati tests
 pytest --doctest-modules amati/
 docker build -t amati -f Dockerfile . 
-cd test
+cd tests/
 docker run --detach -v "$(pwd):/data" amati -d /data
 cd ../
