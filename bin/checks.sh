f8031bf985aff706caf8d3fb6cbcba531fd7c425
Ben <2551337+ben-alexander@users.noreply.github.com>
2025-10-08 14:01:55 +0100

    Run shellcheck

--- bin/checks.sh
+++ bin/checks.sh
@@ -1,9 +1,11 @@
+#!/bin/sh
 ruff check --fix
 ruff format
 python scripts/setup_test_specs.py
 pytest --cov-report term-missing --cov=amati tests
 pytest --doctest-modules amati/
+pyright --verifytypes amati --ignoreexternal --verbose
 docker build -t amati -f Dockerfile . 
-cd tests/
+cd tests/ || exit
 docker run -v "$(pwd):/data" amati -d /data --consistency-check
-cd ../
+cd ..
