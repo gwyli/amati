69873f9f1c4c2a0682c0501d48d34e9d4581ba9c
Ben <2551337+ben-alexander@users.noreply.github.com>
2025-10-08 14:02:53 +0100

    Don't use verbose pyright when checking, can be added if necessary

--- bin/checks.sh
+++ bin/checks.sh
@@ -4,7 +4,7 @@ ruff format
 python scripts/setup_test_specs.py
 pytest --cov-report term-missing --cov=amati tests
 pytest --doctest-modules amati/
-pyright --verifytypes amati --ignoreexternal --verbose
+pyright --verifytypes amati --ignoreexternal
 docker build -t amati -f Dockerfile . 
 cd tests/ || exit
 docker run -v "$(pwd):/data" amati -d /data --consistency-check
